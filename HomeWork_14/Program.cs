d0d32ce76be226cd668b90656b7e97263e730f0b
Anna Goryunova <apelsold@gmail.com>
2022-11-26 19:05:51 +0300

    add files

---
+++ HomeWork_14/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int [] GetArray(int size)
+{
+    int[] array1 = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array1[i] = new Random().Next(0, 1000); 
+    }
+
+    return array1;
+}
+
+int [] array = GetArray(12);
+Console.WriteLine("[" +String.Join(", ",array)+"]");
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+    if (array[i]%2==0) count++;
+
+Console.WriteLine(count);
---
+++ HomeWork_16/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double [] GetArray(int size)
+{
+    double[] array1 = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array1[i] = new Random().NextDouble();
+        array1[i]+=new Random().Next(1, 10);
+        array1[i] = Math.Round(array1[i], 2);
+    }
+
+    return array1;
+}
+
+double [] array = GetArray(5);
+Console.WriteLine("[" +String.Join(", ",array)+"]");
+
+double max = array[0];
+double min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+    if (array[i]<min) min = array[i];
+        else 
+            if (array[i]>max) max = array[i];
+    
+Console.WriteLine("Max = " +max);
+Console.WriteLine("Min = " +min);
+Console.WriteLine("Max - min= " +(max-min));
