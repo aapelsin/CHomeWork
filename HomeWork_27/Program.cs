9fcf3d23fc45d3024702fda4f2595eb85f89945f
Anna Goryunova <apelsold@gmail.com>
2022-12-02 22:17:04 +0300

    add files

---
+++ HomeWork_27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите число больше 1: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(PrintNum(num, 1));
+
+string PrintNum (int start, int end)
+{
+    if (start==end)
+    {
+        return start.ToString();
+    }
+
+    return (start+ " "+ PrintNum (start -1, end));
+
+}
---
+++ HomeWork_29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число от ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число до ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(Sum(num1, num2));
+
+int Sum(int n1, int n2)
+{
+    if (n1 == n2)
+    {
+        return n1;
+    }
+
+    return (n1+Sum(n1 + 1, n2));
+}
